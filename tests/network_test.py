17df49bb30852f9acac4c1655d58b36f13306fac
Fazekas Aron <fazekasaaron2000@gmail.com>
2022-04-10 19:58:56 +0200

    Added planned tests

--- tests/network_test.py
+++ tests/network_test.py
@@ -1,1 +1,81 @@
 from src.components.network import Network
+
+
+def test_network_init():
+    pass
+
+
+def test_network_get_nodes():
+    pass
+
+
+def test_network_get_applications():
+    pass
+
+
+def test_network_get_interfaces():
+    pass
+
+
+def test_network_get_connections():
+    pass
+
+
+def test_network_update_routing_tables():
+    pass
+
+
+def test_network_is_duplicate_node():
+    pass
+
+
+def test_network_get_host():
+    pass
+
+
+def test_network_create_host():
+    pass
+
+
+def test_network_delete_host():
+    pass
+
+
+def test_network_get_router():
+    pass
+
+
+def test_network_create_router():
+    pass
+
+
+def test_network_delete_router():
+    pass
+
+
+def test_network_add_interface():
+    pass
+
+
+def test_network_delete_interface():
+    pass
+
+
+def test_network_set_application():
+    pass
+
+
+def test_network_connect_node_interfaces():
+    pass
+
+
+def test_network_disconnect_node_interfaces():
+    pass
+
+
+def test_network_send_packet():
+    pass
+
+
+def test_network_receive_packet():
+    pass
--- tests/node_test.py
+++ tests/node_test.py
@@ -5,12 +5,92 @@ from src.components.node import Node, Host, Router
 #------------------------------------------------#
 
 
+def test_node_init():
+    pass
+
+
+def test_node_get_best_route_no_match():
+    pass
+
+
+def test_node_get_best_route_found_match():
+    pass
+
+
+def test_node_get_interface():
+    pass
+
+
+def test_node_add_interface():
+    pass
+
+
+def test_node_delete_interface():
+    pass
+
+
+def test_node_connect_to_interface():
+    pass
+
+
+def test_node_disconnect_interface():
+    pass
+
 #------------------------------------------------#
 # HOST TESTS
 #------------------------------------------------#
 
+
+def test_host_init():
+    pass
+
+
+def test_host_set_application():
+    pass
+
+
+def test_host_send_packet():
+    pass
+
+
+def test_host_receive_packet():
+    pass
+
+
+def test_host_handle_feedback():
+    pass
+
+
+def test_host_receive_feedback():
+    pass
+
 # TODO: Test PPV generation properly
+def test_host_something_ppv(): pass
 
 #------------------------------------------------#
 # ROUTER TESTS
 #------------------------------------------------#
+
+
+def test_router_init():
+    pass
+
+
+def test_router_lowest_buffer_ppv():
+    pass
+
+
+def test_router_send_packet():
+    pass
+
+
+def test_router_receive_packet():
+    pass
+
+
+def test_router_send_feedback():
+    pass
+
+
+def test_router_receive_feedback():
+    pass
