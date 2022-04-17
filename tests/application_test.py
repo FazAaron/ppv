a02952f7576e80b40a6bd96e2682da5e0b3e60c5
Fazekas Aron <fazekasaaron2000@gmail.com>
2022-04-17 12:38:54 +0200

    Restructured Application test + added comments

--- tests/application_test.py
+++ tests/application_test.py
@@ -2,153 +2,130 @@ from src.components.application import Application
 from src.components.packet import Packet
 
 
-def test_application_aimd_app_type_init():
+def test_application_init():
     """
-    Test default init behaviour with AIMD app_type
+    Test default init behaviour
     """
+    # Setup an AIMD type Application object
     name = "test_app"
     ip = "127.0.0.1"
     amount = 10
     send_rate = 10
     app_type = "AIMD"
-    app = Application(name, ip, amount, send_rate, app_type)
-    assert app.name == name and \
-        app.ip == ip and \
-        app.amount == amount and \
-        app.send_rate == send_rate and \
-        app.app_type == app_type and \
-        app.curr_sent == 0, \
-        "Application field mismatch during AIMD app_type initialization"
+    app_1 = Application(name, ip, amount, send_rate, app_type)
 
-
-def test_application_const_app_type_init():
-    """
-    Test default init behaviour with CONST app_type
-    """
-    name = "test_app"
-    ip = "127.0.0.1"
-    amount = 10
-    send_rate = 10
+    # Setup a CONST type Application object
     app_type = "CONST"
-    app = Application(name, ip, amount, send_rate, app_type)
-    assert app.name == name and \
-        app.ip == ip and \
-        app.amount == amount and \
-        app.send_rate == send_rate and \
-        app.app_type == app_type and \
-        app.curr_sent == 0, \
-        "Application field mismatch during CONST app_type initialization"
+    app_2 = Application(name, ip, amount, send_rate, app_type)
 
-
-def test_application_other_app_type_init():
-    """
-    Test default init behaviour with any app_type other than AIMD and CONST\n
-    The Application's type will be set to CONST in this case
-    """
-    name = "test_app"
-    ip = "127.0.0.1"
-    amount = 10
-    send_rate = 10
+    # Setup other type Application object
     app_type = "OTHER"
-    app = Application(name, ip, amount, send_rate, app_type)
-    assert app.name == name and \
-        app.ip == ip and \
-        app.amount == amount and \
-        app.send_rate == send_rate and \
-        app.app_type != app_type and \
-        app.app_type == "CONST" and \
-        app.curr_sent == 0, \
-        "Application field mismatch during OTHER app_type initialization"
+    app_3 = Application(name, ip, amount, send_rate, app_type)
 
-
-def test_application_negative_amount_init():
-    """
-    Test default init behaviour with negative amount given\n
-    The Application's amount will be set to 0 in this case
-    """
-    name = "test_app"
-    ip = "127.0.0.1"
+    # Setup negative Packet amount to send on the Application object
     amount = -10
-    send_rate = 10
     app_type = "CONST"
-    app = Application(name, ip, amount, send_rate, app_type)
-    assert app.name == name and \
-        app.ip == ip and \
-        app.amount != amount and \
-        app.amount == 0 and \
-        app.send_rate == send_rate and \
-        app.app_type == app_type and \
-        app.curr_sent == 0, \
-        "Application field mismatch during negative amount initialization"
+    app_4 = Application(name, ip, amount, send_rate, app_type)
+
+    assert app_1.name == "test_app" and \
+        app_1.ip == "127.0.0.1" and \
+        app_1.amount == 10 and \
+        app_1.send_rate == 10 and \
+        app_1.app_type == "AIMD" and \
+        app_1.curr_sent == 0 and \
+        app_2.name == "test_app" and \
+        app_2.ip == "127.0.0.1" and \
+        app_2.amount == 10 and \
+        app_2.send_rate == 10 and \
+        app_2.app_type == "CONST" and \
+        app_2.curr_sent == 0 and \
+        app_3.name == "test_app" and \
+        app_3.ip == "127.0.0.1" and \
+        app_3.amount == 10 and \
+        app_3.send_rate == 10 and \
+        app_3.app_type == "CONST" and \
+        app_3.curr_sent == 0 and \
+        app_4.name == "test_app" and \
+        app_4.ip == "127.0.0.1" and \
+        app_4.amount == 0 and \
+        app_4.send_rate == 10 and \
+        app_4.app_type == "CONST" and \
+        app_4.curr_sent == 0, \
+        "Application field mismatch during initialization"
 
 
 def test_application_send():
     """
-    Test the send() method of the Application\n
-    Every call to send increases curr_sent by 1\n
-    Returns None if the can_send() returns False
+    Test the send() method of the Application
     """
+    # Setup the Application object
     name = "test_app"
     ip = "127.0.0.1"
     amount = 10
     send_rate = 10
     app_type = "CONST"
     app = Application(name, ip, amount, send_rate, app_type)
-    while app.can_send():
+
+    # Send while it can send (while the curr_sent is less than amount)
+    while app.curr_sent < app.amount:
         app.send("127.0.0.1", 1)
-    assert not app.can_send() and \
-        app.curr_sent == amount and \
+
+    assert app.curr_sent == app.amount and \
         app.send("127.0.0.1", 1) is None, \
         "Application.send() failure"
 
 
 def test_application_can_send():
     """
-    Test the can_send() method of the Application\n
-    Returns True if the curr_sent is lower than the amount
+    Test the can_send() method of the Application
     """
+    # Setup the Application object
     name = "test_app"
     ip = "127.0.0.1"
     amount = 10
     send_rate = 10
     app_type = "CONST"
-    app = Application(name, ip, amount, send_rate, app_type)
+    app_1 = Application(name, ip, amount, send_rate, app_type)
+
+    # Send amount - 1 amount of Packets, still making sending available
     for _ in range(amount - 1):
-        app.send("127.0.0.1", 1)
-    assert app.can_send() and app.curr_sent < amount, \
-        "Application.can_send() failure - should be able to send"
+        app_1.send("127.0.0.1", 1)
 
-
-def test_application_cant_send():
-    """
-    Test the can_send() method of the Application\n
-    Returns False if the curr_sent is equal or greater than the amount
-    """
+    # Setup the Application object
     name = "test_app"
     ip = "127.0.0.1"
     amount = 10
     send_rate = 10
     app_type = "CONST"
-    app = Application(name, ip, amount, send_rate, app_type)
+    app_2 = Application(name, ip, amount, send_rate, app_type)
+
+    # Send amount of Packets, making sending not available
     for _ in range(amount):
-        app.send("127.0.0.1", 1)
-    assert not app.can_send() and app.curr_sent == amount, \
-        "Application.can_send() failure - shouldn't be able to send"
+        app_2.send("127.0.0.1", 1)
+
+    assert app_1.can_send() and app_1.curr_sent < amount and \
+        not app_2.can_send() and app_2.curr_sent == amount, \
+        "Application.can_send() failure"
 
 
 def test_application_receive(capsys):
     """
-    Test receiving a Packet on the Application
-    Prints to stdout if a Packet was received
+    Test the receive() method of the Application
     """
+    # Setup the Application object
     name = "test_app"
     ip = "127.0.0.1"
     amount = 10
     send_rate = 10
     app_type = "CONST"
     app = Application(name, ip, amount, send_rate, app_type)
+
+    # Create the Packet to "receive"
     p = Packet("127.0.1.1", "127.0.0.1", 10)
+
+    # Receive the Packet and capture the output
     app.receive(p)
     captured = capsys.readouterr()
+
     assert captured.out == f"Received packet on {app.name}:\n{p}\n", \
         "Application.receive() failure - expected different output"
