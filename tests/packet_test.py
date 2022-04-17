069c315622040b81c6183f942d63e3919d5ca2b2
Fazekas Aron <fazekasaaron2000@gmail.com>
2022-04-17 19:21:26 +0200

    Shorten Packet test and add comments

--- tests/packet_test.py
+++ tests/packet_test.py
@@ -3,44 +3,29 @@ from src.components.packet import Packet
 
 def test_packet_init():
     """
-    Test default init behaviour with proper parameters
+    Test default init behaviour
     """
+    # Setup the Packet object with proper parameters
     source_ip = "192.168.1.1"
     target_ip = "192.168.0.1"
     ppv = 10
-    p = Packet(source_ip, target_ip, ppv)
-    assert p.source_ip == source_ip and \
-        p.target_ip == target_ip and \
-        p.ppv == ppv, "Packet field mismatch during initialization"
+    p_1 = Packet(source_ip, target_ip, ppv)
 
+    # Setup the Packet object with negative PPV
+    ppv_o = -10
+    p_2 = Packet(source_ip, target_ip, ppv_o)
 
-def test_packet_negative_ppv_init():
-    """
-    Test default init behaviour with negative PPV given\n
-    PPV is set to 1 if the value given is below zero or is zero
-    """
-    source_ip = "192.168.1.1"
-    target_ip = "192.168.0.1"
-    ppv = -10
-    p = Packet(source_ip, target_ip, ppv)
-    assert p.source_ip == source_ip and \
-        p.target_ip == target_ip and \
-        p.ppv != ppv and \
-        p.ppv == 1, \
-        "Packet field mismatch during negative PPV initialization"
+    # Setup the Packet object with zero PPV
+    ppv_o = 0
+    p_3 = Packet(source_ip, target_ip, ppv_o)
 
-
-def test_packet_zero_ppv_init():
-    """
-    Test default init behaviour with negative PPV given\n
-    PPV is set to 1 if the value given is below zero or is zero
-    """
-    source_ip = "192.168.1.1"
-    target_ip = "192.168.0.1"
-    ppv = 0
-    p = Packet(source_ip, target_ip, ppv)
-    assert p.source_ip == source_ip and \
-        p.target_ip == target_ip and \
-        p.ppv != ppv and \
-        p.ppv == 1, \
-        "Packet field mismatch during zero PPV initialization"
+    assert p_1.source_ip == source_ip and \
+        p_1.target_ip == target_ip and \
+        p_1.ppv == ppv and \
+        p_2.source_ip == source_ip and \
+        p_2.target_ip == target_ip and \
+        p_2.ppv == 1 and \
+        p_3.source_ip == source_ip and \
+        p_3.target_ip == target_ip and \
+        p_3.ppv == 1, \
+        "Packet field mismatch during initialization"
