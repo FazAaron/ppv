b18c6903184987e9c9a2ef2dec4d84c3120c5f3f
Fazekas Aron <fazekasaaron2000@gmail.com>
2022-05-19 18:02:13 +0200

    Added scrollable Text instead of Label to display data in

--- src/graphic_handlers/object_frame.py
+++ src/graphic_handlers/object_frame.py
@@ -1,7 +1,8 @@
 """
 This module makes ObjectFrame objects available for use when imported
 """
-from tkinter import Button, Label, PhotoImage, ttk
+from tkinter import Button, PhotoImage, ttk
+from tkinter.scrolledtext import ScrolledText
 from idlelib.tooltip import Hovertip
 
 
@@ -16,7 +17,7 @@ class ObjectFrame:
     object_frame       (ttk.Frame): The Frame itself
     exit_button        (Button): Exit button
     exit_button_image  (PhotoImage): The image of the exit_button
-    display_text_label (Label): The field to display the data on
+    display_text_box   (ScrolledText): The field to display the data in
     """
 
     def __init__(self, parent: ttk.Frame) -> None:
@@ -38,19 +39,22 @@ class ObjectFrame:
         self.exit_button: Button = Button(
             self.object_frame, image=self.exit_button_image)
         Hovertip(self.exit_button, "Exit the application")
-        self.display_text_label: Label = Label(
-            self.object_frame, text="", width=10, height=10, font=("JetBrainsMono NF", 10))
+        self.display_text_box: ScrolledText = ScrolledText(
+            self.object_frame, width=10, height=10, font=("JetBrainsMono NF", 10), bg="lightgrey", relief="ridge")
 
         # Set the grid configuration for the Widgets
         self.exit_button.grid(column=1, row=0, sticky="nsew")
-        self.display_text_label.grid(
+        self.display_text_box.grid(
             column=0, row=1, columnspan=2, sticky="nsew", ipadx=0, padx=0)
 
     def display_text(self, to_display: str) -> None:
         """
-        Changes the currently shown text in the display_text_label
+        Changes the currently shown text in the display_text_box
 
         Parameters:
         to_display (str): The string to display
         """
-        self.display_text_label.config(text=to_display)
+        self.display_text_box.config(state="normal")
+        self.display_text_box.delete("1.0", "end")
+        self.display_text_box.insert("end", to_display)
+        self.display_text_box.config(state="disabled")
